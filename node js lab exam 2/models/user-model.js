b772f04fc8efb850d70f3d1ab88a9b5b824b5407
Md. Sajid Salim <55055071+SaJid777@users.noreply.github.com>
2020-02-27 12:56:31 +0600

    Add files via upload

---
+++ node js lab exam 2/models/user-model.js
@@ -0,0 +1,95 @@
+var db = require('./db');
+
+module.exports= {
+	getById : function(id, callback){
+		var sql = "select * from user where id=?";
+		db.getResults(sql, [id], function(results){
+			if(results.length > 0){
+				callback(results[0]);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	getAll : function(callback){
+		var sql = "select * from user";
+		db.getResults(sql, null, function(results){
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback([]);
+			}
+		});
+	},
+	getAllBooks : function(callback){
+		var sql = "select * from book";
+		db.getResults(sql, null, function(results){
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback([]);
+			}
+		});
+	},
+	validate: function(user, callback){
+		var sql ="SELECT * FROM user where username=? and password=?";
+		db.getResults(sql, [user.username, user.password], function(results){
+
+			if(results.length > 0){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	getByUname: function(username, callback){
+		var sql = "select * from user where username=?";
+		db.getResults(sql, [username], function(results){
+			if(results.length > 0){
+				callback(results[0]);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	insert: function(user, callback){
+		var sql = "insert into user values(?,?,?,?)";
+		db.execute(sql, [null, user.username, user.password, user.type], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	insertBook: function(book, callback){
+		var sql = "insert into book values(?,?,?,?,?,?)";
+		db.execute(sql, [null, book.bookname, book.price, book.type, book.author, book.description], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	update : function(user, callback){
+		var sql = "update user set username=?, password=?, type=? where id=?";
+		db.execute(sql, [user.username, user.password, user.type, user.id], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	delete: function(user, callback){
+		var sql = "delete from user where id=?";
+		db.execute(sql, [user.id], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	}
+}
